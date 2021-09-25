9cd870d2fed019a90621c80b137ea48965f714ef
paulomiguelJS <paulomiguelblues@gmail.com>
2021-09-25 00:06:19 -0400

    people.value added and calculateTip function

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const bill = document.getElementById('billTotal');
 const tipBtns = document.querySelectorAll('.tip')
 const customTip = document.getElementById('custom')
+const people = document.getElementById('totalPeople')
+const errorMsg = document.querySelector('.error')
+const results = document.querySelector
 
 bill.addEventListener('input', setBillValue);
 tipBtns.forEach(btn => {
@@ -8,9 +11,12 @@ tipBtns.forEach(btn => {
 })
 
 customTip.addEventListener('input', setTipCustom)
+people.addEventListener('input', setPeople)
 
 let billValue = 0.00; //default value
 let tipValue = 0.15;
+let peopleValue = 1;
+console.log(peopleValue)
 
 
 function validateNumber(s) {
@@ -19,7 +25,7 @@ function validateNumber(s) {
 }
 
 function validateInt(s){
-    let rgx = /^[0-9]9/
+    let rgx = /^[0-9]/
     return s.match(rgx)
 }
 
@@ -30,7 +36,6 @@ function setBillValue() {
 
    
     billValue = parseFloat(bill.value).toFixed(2);
-    console.log(billValue)
 }
 
 function handleClick(event){
@@ -42,6 +47,8 @@ function handleClick(event){
             tipValue = parseFloat(btn.innerHTML)/100
         }
     })
+
+    customTip.value = '';
 }
 
 function setTipCustom(){
@@ -50,5 +57,30 @@ function setTipCustom(){
     }
     tipValue = parseFloat(customTip.value/100)
 
-    console.log(tipValue)
+    tipBtns.forEach(btn => {
+        btn.classList.remove('btnActive')
+    })
 }
+
+    function setPeople(){
+        if(!validateInt(people.value)){
+            people.value = people.value.substring(0, people.value,length-1)
+
+        }
+        peopleValue = parseFloat(people.value)
+
+        if(peopleValue <= 0){
+            errorMsg.classList.add('showError');
+            setTimeout(function(){
+                errorMsg.classList.remove('showError')
+            }, 3000)
+        }
+console.log(peopleValue)
+    }
+
+    function calculateTip(){
+        if(peopleValue >=1) {
+            let tipAmount = billValue * tipValue / peopleValue
+            let total = billValue * (tipValue + 1) / peopleValue
+        }
+    }
